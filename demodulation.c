9fe24553e4f4c6b31bee0ba3b8396b08f259fc3f
L28E <n.ramkhal@gmail.com>
2025-02-21 00:05:05 -0500

    Code cleanup?

--- demodulation.c
+++ demodulation.c
@@ -3,29 +3,39 @@
 #include "demodulation.h"
 #include "wavschema.h"
 
-typedef enum Cell{MARK=0x01, SPACE=0x00} Cell;
-typedef enum State{STOP, READ, RSTOP} State;
+typedef enum Cell
+{
+	MARK = 0x01,
+	SPACE = 0x00
+} Cell;
+typedef enum State
+{
+	STOP,
+	READ,
+	RSTOP
+} State;
 
 /*
  * Function:  demodulate
  * --------------------
  * Demodulates a CUTS encoded WAV file. Here's the block diagram:
- *                                                               _____     
+ *                                                               _____
  *                                                              /     \
  *                      .-----------.       .------------.      /      \
  *              .------>/  Mark BP  /------>/  Envelope  /__/ __/       \
  *              /       /  Filter   /       /  Detector  /      /        \
  *              /       '-----------'       '------------'      /         \
- * FSK input----/                                               / Decision }----> Binary Out 
+ * FSK input----/                                               / Decision }----> Binary Out
  *              /                                               /         /
  *              /       .-----------.       .------------.      /        /
  *              '------>/  Space LP /------>/  Envelope  /__/ __/       /
  *                      /   Filter  /       /  Detector  /      /      /
  *                      '-----------'       '------------'  /   /_____/
- *                                                          /       
- *                                                          Tb      
+ *                                                          /
+ *                                                          Tb
  */
-int demodulate(char inputFile[],char outputFile[],bool plot) {		
+int demodulate(char inputFile[], char outputFile[], bool plot)
+{
 	FILE *inputFilePtr;
 	FILE *plotData;
 	uint32_t dataSize;
@@ -36,7 +46,7 @@ int demodulate(char inputFile[],char outputFile[],bool plot) {
 	double spaceEnvOut;
 	double maxAmpl;
 	double thresholdCoeff = 0.4; // The fraction of the max amplitude to use as the threshold.
-	double threshold; // A cell must have a greater amplitude than this to be detected. Remember that the envelope filters cause some attentuation
+	double threshold;			 // A cell must have a greater amplitude than this to be detected. Remember that the envelope filters cause some attentuation
 	char byte = 0x00;
 	Cell cell = MARK;
 	State state = STOP;
@@ -48,13 +58,14 @@ int demodulate(char inputFile[],char outputFile[],bool plot) {
 	// Assuming we've recorded to a cassette, and then digitally recorded the casette back to a wav.
 	// Open the wav for reading.
 	inputFilePtr = fopen(inputFile, "rb");
-	if (inputFilePtr == NULL) {
+	if (inputFilePtr == NULL)
+	{
 		printf("Something went wrong opening the input file...\n");
 		return -1;
 	}
 
-	/* 	
-		For now, I'll assume all the parameters of this .wav are the same as the one we recorded to the casette, 
+	/*
+		For now, I'll assume all the parameters of this .wav are the same as the one we recorded to the casette,
 		44100 Hz sample rate, 16 bit PCM
 		In that case I just need to get the datasize.
 		Note that because this is 16 bit audio i.e. 2 bytes per sample, the number of samples is half the datasize.
@@ -64,7 +75,7 @@ int demodulate(char inputFile[],char outputFile[],bool plot) {
 	printf("Data size in bytes: %d\n", dataSize);
 
 	// Put all the wav data in a buffer
-	buffer = (int16_t*) malloc(dataSize * sizeof(int16_t));
+	buffer = (int16_t *)malloc(dataSize * sizeof(int16_t));
 	fread(buffer, dataSize, 1, inputFilePtr);
 	fclose(inputFilePtr);
 
@@ -73,83 +84,109 @@ int demodulate(char inputFile[],char outputFile[],bool plot) {
 	hpf = malloc(sizeof(Filter) + sizeof(markImpulse));
 	spaceEnv = malloc(sizeof(Filter) + sizeof(envelopeImpulse));
 	markEnv = malloc(sizeof(Filter) + sizeof(envelopeImpulse));
-	Filter_init(lpf,spaceImpulse,sizeof(spaceImpulse)/sizeof(double));
-	Filter_init(hpf,markImpulse,sizeof(markImpulse)/sizeof(double));
-	Filter_init(spaceEnv,envelopeImpulse,sizeof(envelopeImpulse)/sizeof(double));	 
-	Filter_init(markEnv,envelopeImpulse,sizeof(envelopeImpulse)/sizeof(double));
-	
-	if (plot){
+	Filter_init(lpf, spaceImpulse, sizeof(spaceImpulse) / sizeof(double));
+	Filter_init(hpf, markImpulse, sizeof(markImpulse) / sizeof(double));
+	Filter_init(spaceEnv, envelopeImpulse, sizeof(envelopeImpulse) / sizeof(double));
+	Filter_init(markEnv, envelopeImpulse, sizeof(envelopeImpulse) / sizeof(double));
+
+	if (plot)
+	{
 		// Open a temporary file for plotting
 		plotData = fopen("plot.tmp", "w");
-	}	
-	
+	}
+
 	// Open a file for the bitstream
 	FILE *bitstream = fopen(outputFile, "w");
 
 	// Find the maximum amplitude in the buffer and use that to set the threshold for detection
 	// TODO: Maybe start searching later in the recording to avoid any pops, clicks, etc.
-	maxAmpl=0;
-	for (int i = 0; i < dataSize / 2; i++) {
-		if(buffer[i]>maxAmpl){
-			maxAmpl=buffer[i];
+	maxAmpl = 0;
+	for (int i = 0; i < dataSize / 2; i++)
+	{
+		if (buffer[i] > maxAmpl)
+		{
+			maxAmpl = buffer[i];
 		}
 	}
 	threshold = thresholdCoeff * maxAmpl;
-	
-	// Step through the whole buffer and demodulate it	
-	for (int i = 0; i < dataSize / 2; i++) {
-		
+
+	// Step through the whole buffer and demodulate it
+	for (int i = 0; i < dataSize / 2; i++)
+	{
+
 		// Update the filters
 		Filter_put(lpf, buffer[i]);
-		Filter_put(hpf, buffer[i]);				
+		Filter_put(hpf, buffer[i]);
 
-		// Update the envelope detectors		
+		// Update the envelope detectors
 		Filter_put(spaceEnv, abs(Filter_get(lpf)));
 		Filter_put(markEnv, abs(Filter_get(hpf)));
 
 		// Counter representing Tb, the length of time where each bit is represented. With a bitrate of 44100 Hz each cell is 147 samples long
-		k = (k+1)%samplesPerCell; 
-		if (k == 0){			
+		k = (k + 1) % samplesPerCell;
+		if (k == 0)
+		{
 			// Compare and decide
 			markEnvOut = Filter_get(markEnv);
-			spaceEnvOut = Filter_get(spaceEnv);			
-			
-			if (markEnvOut > spaceEnvOut && markEnvOut >= threshold){
+			spaceEnvOut = Filter_get(spaceEnv);
+
+			if (markEnvOut > spaceEnvOut && markEnvOut >= threshold)
+			{
 				cell = MARK;
-			}else if(spaceEnvOut > markEnvOut && spaceEnvOut >= threshold){
+			}
+			else if (spaceEnvOut > markEnvOut && spaceEnvOut >= threshold)
+			{
 				cell = SPACE;
-			} else{
+			}
+			else
+			{
 				continue;
-			}					
+			}
 
 			// The FSM to decode the bitstream
-			if (state == STOP && cell == SPACE){
-				state = READ; 				// Got a startbit. Move to READ
-			}else if (state == READ){				
-				byte = cell << j | byte; 		// Build the byte
-				j=(j+1)%8; 				// Count cells read
-				if (j==0) state = RSTOP; 		// Move to RSTOP after reading 8 cells
-			}else if (state == RSTOP){
-				if (cell == MARK){					
-					fprintf(bitstream,"%c",byte); 	// Got a stop bit. Append the byte
-					byte = 0x00; 			// Reset the byte					
-					state = STOP; 			// Finished reading the word. Move to STOP
-				}else{
+			if (state == STOP && cell == SPACE)
+			{
+				state = READ; // Got a startbit. Move to READ
+			}
+			else if (state == READ)
+			{
+				byte = cell << j | byte; // Build the byte
+				j = (j + 1) % 8;		 // Count cells read
+				if (j == 0)
+					state = RSTOP; // Move to RSTOP after reading 8 cells
+			}
+			else if (state == RSTOP)
+			{
+				if (cell == MARK)
+				{
+					fprintf(bitstream, "%c", byte); // Got a stop bit. Append the byte
+					byte = 0x00;					// Reset the byte
+					state = STOP;					// Finished reading the word. Move to STOP
+				}
+				else
+				{
 					printf("Error: Expected a stopbit (mark) but got a space!\n");
 					state = STOP;
-				}				
+				}
 			}
-		}		
+		}
 
-		if (plot){
-			// Write to the temp file as we go 
+		if (plot)
+		{
+			// Write to the temp file as we go
 			fprintf(plotData, "%d %d %f %f %f %f %d\n", i, buffer[i], Filter_get(lpf), Filter_get(hpf), Filter_get(spaceEnv), Filter_get(markEnv), cell);
 		}
 	}
-	
-	fclose(bitstream);
 
-	if (plot){
+	fclose(bitstream);
+	free(buffer);
+	free(lpf);
+	free(hpf);
+	free(markEnv);
+	free(spaceEnv);
+
+	if (plot)
+	{
 		fclose(plotData);
 		FILE *gnuPlotPipe = popen("gnuplot -persistent", "w");
 		fprintf(gnuPlotPipe, "%s\n", "plot 'plot.tmp' using 1:2 w l title 'raw', '' using 1:3 w l title 'lowpass', '' using 1:4 w l title 'highpass', '' using 1:5 w l title 'lowenv', '' using 1:6 w l title 'highenv', '' using 1:7 w l title 'bitstream'; pause mouse close");
--- modulation.c
+++ modulation.c
@@ -12,12 +12,14 @@
  * Lets use signed 16 bit PCM. Gives 65536 levels bewteen -32768 and 32767
  */
 void writeCell(int samplesPerCell, double sampleFactor, double volumeFactor,
-		double maxAmpl, FILE *fptr) {
+			   double maxAmpl, FILE *fptr)
+{
 	double pamSample;
 	int16_t pcmSample;
-	for (int t = 0; t < samplesPerCell; t++) {
-		pamSample = sin(sampleFactor * (double) t);
-		pcmSample = (int16_t) (volumeFactor * pamSample * maxAmpl);
+	for (int t = 0; t < samplesPerCell; t++)
+	{
+		pamSample = sin(sampleFactor * (double)t);
+		pcmSample = (int16_t)(volumeFactor * pamSample * maxAmpl);
 		fwrite(&pcmSample, 2, 1, fptr);
 	}
 }
@@ -29,21 +31,26 @@ void writeCell(int samplesPerCell, double sampleFactor, double volumeFactor,
  * The byte is written to the tape in little endian.
  */
 void writeFrame(int samplesPerCell, double markSampleFactor,
-		double spaceSampleFactor, double volumeFactor, double maxAmpl,
-		FILE *fptr, char byte) {
+				double spaceSampleFactor, double volumeFactor, double maxAmpl,
+				FILE *fptr, char byte)
+{
 	uint8_t mask = 0x01; // 0b00000001 , a bit mask for the least significant bit
 
 	// Write the startbit (Space)
 	writeCell(samplesPerCell, spaceSampleFactor, volumeFactor, maxAmpl, fptr);
 
 	// Write the byte (in little endian)
-	for (int i = 0; i < 8; ++i) {
-		if (mask << i & byte) {
+	for (int i = 0; i < 8; ++i)
+	{
+		if (mask << i & byte)
+		{
 			writeCell(samplesPerCell, markSampleFactor, volumeFactor, maxAmpl,
-					fptr);
-		} else {
+					  fptr);
+		}
+		else
+		{
 			writeCell(samplesPerCell, spaceSampleFactor, volumeFactor, maxAmpl,
-					fptr);
+					  fptr);
 		}
 	}
 
@@ -57,7 +64,8 @@ void writeFrame(int samplesPerCell, double markSampleFactor,
  * Converts an inputfile into a CUTS encoded WAV file
  *
  */
-int modulate(char inputFile[],char outputFile[]) {	
+int modulate(char inputFile[], char outputFile[])
+{
 	double pamSample;
 	int16_t pcmSample;
 	double maxAmpl = pow(2, bitDepth - 1) - 1;
@@ -67,7 +75,7 @@ int modulate(char inputFile[],char outputFile[]) {
 	WavMetadata meta;
 	FILE *outputFilePtr, *inputFilePtr;
 
-	// Open the file for reading and get its length in bytes	
+	// Open the file for reading and get its length in bytes
 	inputFilePtr = fopen(inputFile, "rb");
 	if (inputFilePtr == NULL)
 		return -1;
@@ -76,7 +84,7 @@ int modulate(char inputFile[],char outputFile[]) {
 	rewind(inputFilePtr);
 
 	// Allocate memory for and read for the whole file.
-	inputBuffer = (uint8_t*) malloc(inputFilelen * sizeof(uint8_t));
+	inputBuffer = (uint8_t *)malloc(inputFilelen * sizeof(uint8_t));
 	fread(inputBuffer, inputFilelen, 1, inputFilePtr);
 	fclose(inputFilePtr);
 
@@ -89,28 +97,30 @@ int modulate(char inputFile[],char outputFile[]) {
 	/* 	- Each byte in the input file is encoded with 10 cells
 			- Each cell has 147 samples
 			- Each sample is 2 bytes
-	*/  
-	double dataSize = inputFilelen * 10 * samplesPerCell * 2; 
+	*/
+	double dataSize = inputFilelen * 10 * samplesPerCell * 2;
 
 	// Populate the header and write it to the file.
 	writeMeta(&meta, leadSize + dataSize, sampleRate, numChannels, bitDepth);
 	fwrite(&meta, sizeof(meta), 1, outputFilePtr);
 
 	// Start /w 5 seconds of mark.
-	for (int t = 0; t < 5 * sampleRate; t++) {
-		pamSample = sin(markSampleFactor * (double) t);
-		pcmSample = (int16_t) (volumeFactor * pamSample * maxAmpl);
+	for (int t = 0; t < 5 * sampleRate; t++)
+	{
+		pamSample = sin(markSampleFactor * (double)t);
+		pcmSample = (int16_t)(volumeFactor * pamSample * maxAmpl);
 		fwrite(&pcmSample, 2, 1, outputFilePtr);
 	}
 
 	// Write the data.
-	for (int i = 0; i < inputFilelen; i++) {
+	for (int i = 0; i < inputFilelen; i++)
+	{
 		writeFrame(samplesPerCell, markSampleFactor, spaceSampleFactor,
-				volumeFactor, maxAmpl, outputFilePtr, inputBuffer[i]);
+				   volumeFactor, maxAmpl, outputFilePtr, inputBuffer[i]);
 	}
 
 	// Close the wav file.
 	fclose(outputFilePtr);
+	free(inputBuffer);
 	return 0;
-
 }
